6ebdef5cca3dded6816ba5141a34b402d8a8dc41
Endy Tjahjono <endy.tjahjono@verint.com>
2023-03-24 09:01:50 +0700

    Tell the computer to speak.

--- SayThingsPeriodically.ps1
+++ SayThingsPeriodically.ps1
@@ -1,7 +1,7 @@
 Add-Type -AssemblyName System.Speech
 while (1) {
     (New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer).Speak("Don't get stuck, keep moving!")
-    $delay = Get-Random -Minimum 600 -Maximum 1800
+    $delay = Get-Random -Minimum 300 -Maximum 900
     Write-Output "$($delay) seconds delay. Will play next at $((Get-Date).AddSeconds($delay))"
     Start-Sleep -Seconds $delay
     #break
